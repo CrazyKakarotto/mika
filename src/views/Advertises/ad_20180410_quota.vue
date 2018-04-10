d696b83eebc01d7caec228811a33d94314c53d63
yansheng <17610165692@163.com>
2018-04-10 20:45:09 +0800

    mika

---
+++ src/views/Advertises/ad_20180410_quota.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="advertise">
+    <!-- <img v-lazy="require('../../assets/img/advertise/20180105/aletter_flag.png')" /> -->
+  </div>
+</template>
+
+<script>
+  import {BaiduTongji} from "../../utils/BaiduTongji";
+
+  export default {
+    name: "ad_20180410_quota",
+    mounted() {
+      BaiduTongji();
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .advertise {
+    width: 100%;
+    min-height: 100vh;
+    background: url("../../assets/img/advertise/20180410/quota_notice.png") top center no-repeat;
+    background-size: cover;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+      width: 100%;
+      margin-top: 3.15rem;;
+      margin-bottom: 1rem;
+    }
+    img[lazy=loading] {
+      width: 0.8rem;
+      margin: auto;
+    }
+  }
+
+</style>
